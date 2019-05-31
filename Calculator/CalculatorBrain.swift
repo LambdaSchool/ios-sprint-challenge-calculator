2c9f4d2887d27e420969b82c9a10e5d8330621c9
Jake Connerly <jakeconnerly@Macintosh.local>
2019-05-31 12:07:16 -0500

    Added enum operatorType built CAlculatorBrain class methods addOperand and calculateIfPossible

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -7,20 +7,40 @@
 //
 
 import Foundation
-
+enum OperatorType: String {
+    case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
+}
 class CalculatorBrain {
-    var operand1String = ""
-    var operand2String = ""
-    
-    func addOperandDigit(_ digit: String) -> String {
-        
+    var operand1String: String?
+    var operand2String: String?
+
+    func addOperandDigitOne(_ digit1: String) -> String {
+        return digit1
     }
-    
+    func addOperandDigitTwo(_ digit2: String) -> String {
+        return digit2
+    }
+
     func setOperator(_ operatorString: String) {
-        
+        let operatorType: OperatorType?
     }
-    
-    func calculateIfPossible() -> String? {
+
+    func calculateIfPossible(digit1: Int, digit2: Int, operatorSelected: OperatorType) -> String? {
+        var result = 0
+        switch operatorSelected {
+        case .addition:
+            result = digit1 + digit2
+        case .subtraction:
+            result = digit1 - digit2
+        case .multiplication:
+            result = digit1 * digit2
+        case .division:
+            result = digit1 / digit2
+        }
         
+        return String(result)
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -10,20 +10,50 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    let calcBrain = CalculatorBrain()
+    
     @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var numOneButton: UIButton!
+    @IBOutlet weak var numTwoButton: UIButton!
+    @IBOutlet weak var numThreeButton: UIButton!
+    @IBOutlet weak var numFourButton: UIButton!
+    @IBOutlet weak var numFiveButton: UIButton!
+    @IBOutlet weak var numSixButton: UIButton!
+    @IBOutlet weak var numSevenButton: UIButton!
+    @IBOutlet weak var numEightButton: UIButton!
+    @IBOutlet weak var numNineButton: UIButton!
+    @IBOutlet weak var numZeroButton: UIButton!
+    @IBOutlet weak var decimalButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
     }
     
     // MARK: - Action Handlers
     
-    @IBAction func operandTapped(_ sender: UIButton) {
+    @IBAction func operandTapped(_ choiceSender: UIButton) {
+        var firstNumber = ""
+        if let uNumberOne = choiceSender.currentTitle {
+            firstNumber = String(uNumberOne)
+        }
         
+        outputLabel.text = calcBrain.addOperandDigitOne(firstNumber)
+        
+        
+        //var secondNumber = ""
+        //calcBrain.operand2String = choiceSender.currentTitle
+        //if let uNumberTwo = calcBrain.operand2String {
+        //
+        //    secondNumber = uNumberTwo
+        //}
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
         
+        calcBrain.setOperator(<#T##operatorString: String##String#>)
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
@@ -31,12 +61,13 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        outputLabel.text = String(0)
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        calcBrain.operand1String = "0"
+        calcBrain.operand2String = "0"
     }
 }

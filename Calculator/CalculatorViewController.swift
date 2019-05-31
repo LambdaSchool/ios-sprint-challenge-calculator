0e80728716cd89112b4d39f5593d39e53dc8b2d5
Marlon Raskin <marlonjraskin@gmail.com>
2019-05-31 18:10:45 -0400

    fixed code in CalculatorBrain and CalculatorViewController. cleaned up code. Added function for solution.

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -10,39 +10,47 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 	
-	var calculatorBrain: CalculatorBrain?
+	var brain: CalculatorBrain?
 	
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-		calculatorBrain = CalculatorBrain()
+		brain = CalculatorBrain()
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
 		if let numberOperand = sender.titleLabel?.text {
-			outputLabel.text = calculatorBrain?.addOperandDigit(numberOperand)
+			outputLabel.text = brain?.addOperandDigit(numberOperand)
 		}
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-		
+		if let operatorButton = sender.titleLabel?.text {
+			brain?.setOperator(operatorButton)
+		}
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+		if let solution = brain?.calculateIfPossible() {
+			outputLabel.text = solution
+			clearTransaction()
+		}
     }
-    
+
     @IBAction func clearTapped(_ sender: UIButton) {
 		clearTransaction()
+		outputLabel.text = "0"
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        outputLabel.text = ""
-		calculatorBrain = CalculatorBrain()
+		brain = nil
+		brain = CalculatorBrain()
     }
 }
+
+//Changed: brain name, added `brain = nil` before `brain CalculatorBrain?` on line 51, added line 52 (from instructor), added line 37, 38, & 39 (Instructor)

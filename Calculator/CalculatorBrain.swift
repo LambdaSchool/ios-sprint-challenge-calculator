6efb25162121dd3f8b84b454e1341cff777f0dad
shillwil <alex.shillingford@icloud.com>
2019-07-12 10:49:55 -0600

    operandTapped now changes label to operand1String. clearTapped() wipes brain memory and changes label back to 0.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
@@ -18,7 +21,19 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType != nil {
+            if operand1String.isEmpty {
+                operand1String = "0"
+                operand2String += digit
+                return operand2String
+            } else {
+                operand2String += digit
+                return operand2String
+            }
+        } else {
+            operand1String += digit
+            return operand1String
+        }
     }
     
     func setOperator(_ operatorString: String) {
@@ -26,6 +41,6 @@ class CalculatorBrain {
     }
     
     func calculateIfPossible() -> String? {
-        
+        return nil
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -12,14 +12,21 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
+    var brain: CalculatorBrain?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        brain = CalculatorBrain()
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        outputLabel.text = "0"
+        if let brain = brain,
+            let operand = sender.currentTitle {
+            outputLabel.text = brain.addOperandDigit(operand)
+        }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
@@ -31,12 +38,17 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
+        guard let brain = brain else { return }
         
+        brain.operand1String = ""
+        brain.operand2String = ""
+        brain.operatorType = nil
+        outputLabel.text = "0"
     }
 }

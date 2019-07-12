5f3e7c041a94683913d9e143216b11977a00a551
Steven <Sleyva1991@outlook.com>
2019-07-12 13:13:17 -0400

    Started on CalculatorBrain code, added the rest of the cases in the enum and added functionallity to addOperandDigit

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
@@ -18,7 +21,13 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType != nil {
+            operand2String += digit
+            return operand2String
+        } else {
+            operand1String += digit
+            return operand1String
+        }
     }
     
     func setOperator(_ operatorString: String) {

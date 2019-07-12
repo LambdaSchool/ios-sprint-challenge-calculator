646cccf2e1a79573d7c1d894ce191eeb782052d9
Taylor Lyles <hawks93187@gmail.com>
2019-07-12 09:07:50 -0700

    Added if statment about operatorType and figured out what digits go in function addOperandDigit

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -21,7 +21,13 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+		if operatorType != nil {
+			operand1String += digit
+			return operand1String
+		} else  {
+			operand2String += digit
+			return operand2String
+		}
     }
     
     func setOperator(_ operatorString: String) {

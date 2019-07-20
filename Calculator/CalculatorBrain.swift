9fc9df58b601902d2aec9fd728b96185678f48f9
Clayton Bonaguidi <claytonbonaguidi@gmail.com>
2019-07-19 21:06:58 -0700

    MVP Sprint 1

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -6,10 +6,54 @@
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
+//class DepartureBoard {
+//    var departureFlights: String
+//    var currentAirport: String
+//    private (set) var flights: [JFKAirports]
+//
+//    init(departureFlights: String, currentAirport: String) {
+//        self.departureFlights = departureFlights
+//        self.currentAirport = currentAirport
+//        self.flights = []
+//
+//    }
+//
+//    func alertPassengers() {
+//        for flight in flights {
+//            if flight.terminal == nil {
+//                "TBD"
+//            } else {
+//                switch flight.flightStatus {
+//                case .canceled:
+//                    print("We're sorry your flight to \(flight.destination) was canceled, here is a $500 voucher")
+//                case .boarding:
+//                    print("Your flight is boarding, please head to terminal: \(flight.terminal!) immediately. The doors are closing soon.")
+//                case .scheduled:
+//                    print("Your flight to \(flight.destination) is scheduled to depart at \(flight.departureTime!) from terminal: \(flight.terminal!)")
+//                case .delayed:
+//                    print("Im sorry your flight was delayed")
+//                case .enRoute:
+//                    print("Your flight is En Route, YAY!")
+//                }
+//            }
+//        }
+//    }
+//
+//
+//    func add(flight: JFKAirports) {
+//        flights.append(flight)
+//    }
+//}
+//
+
+
 import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplaction = "*"
+    case division = "/"
 }
 
 class CalculatorBrain {
@@ -17,8 +61,18 @@ class CalculatorBrain {
     var operand2String = ""
     var operatorType: OperatorType?
     
+    init(operand1String: String, operand2String: String, operatorType: String) {
+        self.operand1String = operand1String
+        self.operand2String = operand2String
+        self.operatorType = OperatorType(rawValue: operatorType)
+    }
     func addOperandDigit(_ digit: String) -> String {
+        for 
+       
         
+        
+        
+        CalculatorBrain(operand1String: "", operand2String: "", operatorType: "\(OperatorType.self)")
     }
     
     func setOperator(_ operatorString: String) {
@@ -26,6 +80,6 @@ class CalculatorBrain {
     }
     
     func calculateIfPossible() -> String? {
-        
+       
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -10,28 +10,107 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var operandOnScreen: Double = 0
+    var previousNumber: Double = 0
+    var operandCalculated = false
+    var operators = 0
+    
     @IBOutlet weak var outputLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        class CalculatorBrain {
+            var operand1 = Double()
+            var operand2 = Double()
+            var operators = Double()
+            
+            init(operand1: Double, operand2: Double, operators: Double) {
+                self.operand1 = operand1
+                self.operand2 = operand2
+                self.operators = operators
+            }
+            
+        }
     }
-    
+    var brain: CalculatorBrain?
     // MARK: - Action Handlers
     
-    @IBAction func operandTapped(_ sender: UIButton) {
+    @IBAction func operandTapped(_ sender: UIButton)
+    {
+        if operandCalculated == true
+        {
+                outputLabel.text = String(sender.tag-1)
+                operandOnScreen = Double(outputLabel.text!)!
+                operandCalculated = false
+        }
         
+        outputLabel.text = outputLabel.text! + String(sender.tag-1)
+        
+        operandOnScreen = Double(outputLabel.text!)!
     }
     
-    @IBAction func operatorTapped(_ sender: UIButton) {
-        
+    @IBAction func operatorTapped(_ sender: UIButton)
+    {
+        if outputLabel.text != "" && sender.tag != 16
+        {
+            previousNumber = Double(outputLabel.text!)!
+            if sender .tag == 12
+            { //Division
+                outputLabel.text = "/"
+            }
+            else if sender .tag == 13
+            { //Multiple
+                outputLabel.text = "*"
+            }
+            else if sender .tag == 14
+            { //Subtraction
+                outputLabel.text = "-"
+            }
+            else if sender .tag == 15
+            { // Addition
+                outputLabel.text = "+"
+            }
+            operandCalculated = true
+            operators = sender.tag
+            
+            }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        if sender.tag == 16
+        {
+            if operators == 12
+            {
+                outputLabel.text = String(previousNumber / operandOnScreen)
+            }
+            else if operators == 13
+            {
+                outputLabel.text = String(previousNumber * operandOnScreen)
+            }
+            else if operators == 14
+            {
+                outputLabel.text = String(previousNumber - operandOnScreen)
+            }
+            else if operators == 15
+            {
+                outputLabel.text = String(previousNumber + operandOnScreen)
+            }
+        }
+        else if sender.tag == 11
+        {
+            outputLabel = nil
+            previousNumber = 0
+            operandOnScreen = 0
+        }
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        if sender.tag == 11 {
+            outputLabel = nil
+            previousNumber = 0
+            operandOnScreen = 0
+        }
     }
     
     // MARK: - Private
@@ -40,3 +119,5 @@ class CalculatorViewController: UIViewController {
         
     }
 }
+
+

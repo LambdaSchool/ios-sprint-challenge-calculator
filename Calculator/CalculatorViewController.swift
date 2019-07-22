837a5e8c452c0507002ce064f278dc2df0d5b9d9
Jessie Griffin <i.jessie.me@icloud.com>
2019-07-21 23:20:49 -0400

    delete unnecessary enum

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -12,19 +12,6 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
-    enum Number: String {
-        case zero = "0"
-        case one = "1"
-        case two = "2"
-        case three = "3"
-        case four = "4"
-        case five = "5"
-        case six = "6"
-        case seven = "7"
-        case eight = "8"
-        case nine = "9"
-    }
-    
     var brain: CalculatorBrain?
     
     override func viewDidLoad() {

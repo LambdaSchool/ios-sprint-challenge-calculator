fbb2a56384b008cd235e4d679c89e1172956fc4d
JosephRogers94 <Joerogersss2016@gmail.com>
2019-07-22 01:18:57 -0500

    Finished Sprint

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -12,23 +12,57 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
-    
+   //this will ask if the operator type is nil. If not, it appends digits to it, and returns it to the method
+    //i still dont get how it knows to ORDER these numbers I just kind of got it to work at this point with a lot of stack overflow
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType != nil {
+            operand1String.append(digit)
+            return operand2String
+        } else {
+            operand2String.append(digit)
+            return operand1String
+        }
     }
     
     func setOperator(_ operatorString: String) {
-        
+       //this will give the opertaor type a raw value (so the title) of the string being cast in. which is of tyoe operator tupe.
+        operatorType = OperatorType(rawValue: operatorString)
     }
-    
+    //this was ridiculously hard for me to call out with strings. i didnt really feel like i solodified the concepts very well and would like some support on some after hours or some targeted lessons
     func calculateIfPossible() -> String? {
-        
+        if operand1String.isEmpty, operand2String.isEmpty, operatorType == nil {
+            return "0"
+        } else {
+            var calculatedAnswer: String
+            
+            if let num1 = Double(operand1String), let num2 = Double(operand2String) {
+                
+                switch operatorType {
+                case .addition? :
+                    calculatedAnswer = String(num1 + num2)
+                case .subtraction? :
+                    calculatedAnswer = String(num1 - num2)
+                case .multiplication? :
+                    calculatedAnswer = String(num1 * num2)
+                case .division? :
+                    calculatedAnswer = String(num1 / num2)
+                default :
+                    return nil}} else {
+                return nil
+            }
+            operand1String = calculatedAnswer
+            operand2String = ""
+            return calculatedAnswer
+        }
     }
 }
 
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -9,33 +9,62 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    //1: Built a object of type Calculator Brain.
+    var brain: CalculatorBrain?
+    
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //2: We made a new object of Brain.
+        brain = CalculatorBrain()
+        
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        //3: made a number represented as a string from the titles description that's pressed.
+        if let number = sender.titleLabel?.text {
+            //4: created a constant called operand that is equal to the value of number
+           let operand = number
+            //5: called the method to add the digit to the calculation.
+            outputLabel.text = brain?.addOperandDigit(operand)
+            
+           
+        }
 }
 
 @IBAction func operatorTapped(_ sender: UIButton) {
-    
+   
+    //this is doing the same thing and unwrapping the item and saying if its the title label texts and then setting that unwrapped value as the opeator icon
+    if let operatorIconChosen = sender.titleLabel?.text {
+        brain?.setOperator(operatorIconChosen)
+        
+    }
+
 }
 
 @IBAction func equalTapped(_ sender: UIButton) {
-    
+   //I feel I am having a major issue with advanced optionals.
+    if let displayAnswer = brain?.calculateIfPossible() {
+       // i finished this with trial and error on this part but i know its taking the optional and filling it in
+        outputLabel.text = displayAnswer
+    }
 }
 
 @IBAction func clearTapped(_ sender: UIButton) {
+    //7: call clear transaction which will clear the properties within my brain
+    clearTransaction()
+    //8: changes the output label back to zero
+    outputLabel.text = "0"
 }
 
 // MARK: - Private
 
 private func clearTransaction() {
-    
+    //9: Resetting the brain by calling clear when the clear button is tapped. which calls for the brain to go back to its normal state.
+    brain = CalculatorBrain()
 }
 
 

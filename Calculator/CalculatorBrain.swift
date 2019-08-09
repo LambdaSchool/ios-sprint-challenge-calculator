c9c04c0e305bfb3f484529dcc342b857e6140508
Joshua Sharp <sharp.josh@gmail.com>
2019-08-09 12:37:25 -0400

    pre-output handling change
    changing the way the display output is handled

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,15 +10,29 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
 }
 
 class CalculatorBrain {
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
+    var outputDisplay:String = "0"
     
     func addOperandDigit(_ digit: String) -> String {
+        if operatorType == nil {
+            if !operand1String.contains(".") {
+                operand1String += digit
+            }
+        } else {
+            if !operand2String.contains(".") {
+                operand2String += digit
+            }
+        }
         
+        return digit
     }
     
     func setOperator(_ operatorString: String) {
@@ -26,6 +40,13 @@ class CalculatorBrain {
     }
     
     func calculateIfPossible() -> String? {
-        
+        return ""
+    }
+    
+    func clear () {
+        operand1String = ""
+        operand2String = ""
+        operatorType = nil
+        outputDisplay = "0"
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var calculator = CalculatorBrain()
+    
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
@@ -19,24 +21,42 @@ class CalculatorViewController: UIViewController {
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        guard let digit = sender.titleLabel?.text else {return}
+        var uwOutputLabel = outputLabel.text ?? ""
+        if uwOutputLabel == "0" { uwOutputLabel = "" }
+        outputLabel.text = uwOutputLabel + calculator.addOperandDigit(digit)
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        guard let op = sender.titleLabel?.text else { return }
+        let uwOutputLabel = outputLabel.text ?? ""
+        if uwOutputLabel != "" {
+            if uwOutputLabel != "0" {
+                calculator.setOperator (op)
+                outputLabel.text = uwOutputLabel + op
+            } else {
+                calculator.addOperandDigit("0")
+                calculator.setOperator(op)
+            }
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        guard let result = calculator.calculateIfPossible() else {
+            outputLabel.text = "Error"
+            return
+        }
+        outputLabel.text = result
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        calculator.clear()
+        outputLabel.text = "0"
     }
 }

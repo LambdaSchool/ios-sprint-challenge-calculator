eba497c2ae42e844023bff7cb63f7ec4cac976b8
DaniBlackwell <bleuxgenius@gmail.com>
2019-08-09 15:27:11 -0500

    Update CalculatorViewController.swift

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -18,49 +18,83 @@ class CalculatorViewController: UIViewController {
   
     @IBOutlet weak var outputLabel: UILabel!
     
-   var firstnumber = 0
+    var firstnumber = 0
     var secondNumber = 0
-    var operan = 0
+    var operand = 0
     var answer : Double = 0.0
     
-    var number : String = "0"
+    var theNumber : String = "0"
     
-    func saveNumber() {
-        firstnumber = Int(number)!
-        number = "0"
-        
+    func saveFirstNumber() {
+        firstnumber = Int(theNumber)!
+        theNumber = "0"
+        printNumber()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print((number))
+        
+   printNumber()
+    }
+    
+    func printNumber () {
+        outputLabel.text = theNumber
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
         if sender.tag >= 0 && sender.tag <= 9 {
-            number += String(sender.tag)
-            print(number)
+            theNumber += String(sender.tag)
+            printNumber()
         }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        if sender.tag >= 10 && sender.tag <= 13 {
+            operand = sender.tag
+            saveFirstNumber()
+        }
+        if sender.tag == -3 {
+            theNumber = "0"
+            printNumber()
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
+        secondNumber = Int(theNumber) ?? 0
         
+        if operand == Add {
+        answer = Double(firstnumber + secondNumber)
     }
-    
-    @IBAction func clearTapped(_ sender: UIButton) {
+        if operand == minus {
+            answer = Double(firstnumber - secondNumber)
+        }
+        if operand == mulptiply {
+            answer = Double(firstnumber * secondNumber)
+        }
+        if operand == divide {
+        
+            answer = Double(firstnumber) / Double(secondNumber)
+        
+        }
+        
+        firstnumber = 0
+        secondNumber = 0
+        operand = Add
+        theNumber = String(answer)
+        printNumber()
+        
+        answer = 0.0
+        theNumber = "0"
+        
+        func clearTapped(_ sender: UIButton) {
         
     }
     
     // MARK: - Private
     
-    private func clearTransaction() {
-        outputLabel.text = number
+     func clearTransaction() {
     }
 }
-
+}

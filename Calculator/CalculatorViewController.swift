8406d714db1f9c35ddb85fc7b5972c8134de523f
RoastBeefKazenakis <sabinowitz@gmail.com>
2019-08-09 13:40:54 -0400

    Got the outlets connected, finally..

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button0: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +32,26 @@ let brain = CalculatorBrain(operand1String: "", operand2String: "", operatortype
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
+        var operand: String? = nil
+        sender.isSelected.toggle()
+        if let pressedOperand = String?(operand) {
         
+        }
     }
+
+    
+//operatorTapped function
     
     @IBAction func operatorTapped(_ sender: UIButton) {
         
     }
+
+// equalTappedFunction
     
     @IBAction func equalTapped(_ sender: UIButton) {
-    calculateif
+//    brain.calculateIfPossible()
+//    outputLabel.text
     }
-    
     @IBAction func clearTapped(_ sender: UIButton) {
         
     }

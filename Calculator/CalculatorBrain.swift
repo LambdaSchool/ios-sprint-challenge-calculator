2429ad8055fdeab9b7bab17c6a827e35ddfc5819
bfvilchez <bfvilchez@icloud.com>
2019-08-09 11:00:31 -0600

    finished sprint challenge

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case mutliply = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
@@ -18,14 +21,36 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType == nil {
+            operand1String += digit
+            print(operand1String)
+            return operand1String
+        } else if operatorType != nil {
+            operand2String += digit
+            print(operand2String)
+            return operand2String
+        }
+       return ""
     }
     
     func setOperator(_ operatorString: String) {
-        
+     let operatorType = OperatorType.init(rawValue: operatorString)
+        self.operatorType = operatorType
+       
     }
     
     func calculateIfPossible() -> String? {
+        guard let operatorType = operatorType,
+        let operand1 = Double(operand1String),
+        let operand2 = Double(operand2String) else {return ""}
         
+        switch operatorType {
+        case .addition: return "\(operand1 + operand2)"
+        case .subtraction: return "\(operand1 - operand2)"
+        case .mutliply : return "\(operand1 * operand2)"
+        case .division: return "\(operand1 / operand2)"
+      
+        }
+
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -11,32 +11,47 @@ import UIKit
 class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
+    var brain: CalculatorBrain?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        brain = CalculatorBrain()
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
+        guard let brain = brain, let number = sender.titleLabel?.text else {return}
+        outputLabel.text = "\(brain.addOperandDigit(number))"
         
-    }
+    
+        }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        guard let brain = brain, let operatorType = sender.titleLabel?.text else {return}
+        brain.setOperator(operatorType)
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
+        guard let brain = brain else {return}
+        if let ArithmeticSolution = brain.calculateIfPossible() {
+            print(ArithmeticSolution)
+            outputLabel.text = "\(ArithmeticSolution)"
+        }
+        
+        
         
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        outputLabel.text = "0"
+        brain = nil
+        brain = CalculatorBrain()
     }
 }

4a88e6937c42ce5771288c59e7f9c854b6ae5beb
Jonalynn Masters <jonalynna.masters@gmail.com>
2019-09-06 11:36:34 -0700

    final commit

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
 }
 
 class CalculatorBrain {
@@ -17,15 +20,44 @@ class CalculatorBrain {
     var operand2String = ""
     var operatorType: OperatorType?
     
+    
     func addOperandDigit(_ digit: String) -> String {
+        if let _ = operatorType {
+            operand2String += digit
+            return operand2String
+            } else {
+                operand1String += digit
+                return operand1String
+            }
         
     }
     
     func setOperator(_ operatorString: String) {
-        
+        if let operandType = OperatorType(rawValue: operatorString) {
+            operatorType = operandType
+        }
     }
     
     func calculateIfPossible() -> String? {
-        
+        if operand1String != "",
+            operand2String != "",
+            let type = operatorType,
+            let operand1 = Double(operand1String),
+            let operand2 = Double(operand2String) {
+            
+            switch type {
+            case .addition:
+                return ("\(operand1 + operand2)")
+            case .subtraction:
+                return ("\(operand1 - operand2)")
+            case .multiplication:
+                return ("\(operand1 * operand2)")
+            case .division:
+                return ("\(operand1 / operand2)")
+            }
+        }
+        return " "
     }
+    
+
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -12,31 +12,41 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
+    var brain: CalculatorBrain?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        brain = CalculatorBrain ()
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        if let digit = sender.titleLabel?.text {
+            outputLabel.text = brain?.addOperandDigit(digit)
+        }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        if let operatorDigit = sender.titleLabel?.text {
+            brain?.setOperator(operatorDigit)
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        if let result = brain?.calculateIfPossible() {
+            outputLabel.text = result
+        }
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
+        outputLabel.text = "0"
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = CalculatorBrain()
     }
 }

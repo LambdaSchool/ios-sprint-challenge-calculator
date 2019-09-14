99944ed93db5fe1cb021b9a5cccc0dfc094e333e
Bree <bree.j.32@gmail.com>
2019-09-13 21:40:36 -0400

    did step 1

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,12 +10,23 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
 }
 
 class CalculatorBrain {
+    var brain: Int?
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
+    
+    init(brain: Int?, _ operand1String: String?, _ operand2String: String?, operatorType: OperatorType?) {
+        self.brain = brain
+        self.operand1String = operand1String!
+        self.operand2String = operand2String!
+        self.operatorType = operatorType
+    }
     
     func addOperandDigit(_ digit: String) -> String {
         

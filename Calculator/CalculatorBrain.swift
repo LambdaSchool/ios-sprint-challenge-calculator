deab817c7fccb3b23018386b0729bb0e7d0acf42
JosephRogers94 <Joerogersss2016@gmail.com>
2019-09-15 10:27:42 -0500

    Added Comments

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -14,7 +14,7 @@
 import Foundation
 
 
-
+//here we are creating the enum for different operator types. this way we can easily access them through the enum type. 
 enum OperatorType: String {
     case addition = "+"
     case subtraction = "−"
@@ -27,7 +27,7 @@ class CalculatorBrain {
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
-   //this will ask if the operator type is nil. If not, it appends digits to it, and returns it to the method
+    //this will ask if the operator type is nil. If not, it appends digits to it, and returns it to the method
     //i still dont get how it knows to ORDER these numbers I just kind of got it to work at this point with a lot of stack overflow
     func addOperandDigit(_ digit: String) -> String {
         if operatorType != nil {
@@ -40,18 +40,25 @@ class CalculatorBrain {
     }
     
     func setOperator(_ operatorString: String) {
-       //this will give the opertaor type a raw value (so the title) of the string being cast in. which is of tyoe operator tupe.
+        //this will give the opertaor type a raw value (so the title) of the string being cast in. which is of tyoe operator tupe.
         operatorType = OperatorType(rawValue: operatorString)
     }
-    //this was ridiculously hard for me to call out with strings. i didnt really feel like i solodified the concepts very well and would like some support on some after hours or some targeted lessons
+    
+            //1 this is asking if the operand 1 & 2 are not empty. as well as if the operator type is nil. if so, return 0. If not. then continue to the else statment.
+       //2 this is creating a variable acalled calculatedAnswer which is of type String. this is so we can assign the answer to this value and return it within the function.
+    //3 create a constant called num1 and num2 that are of type Double casted operandString 1 & 2 respectively. if so, then continue to the switch statement.
+    //4 the switch statement here is checking the operator type that has been chosen. And using the numbers assigned to do the respective calculations for the different buttons.
+    
     func calculateIfPossible() -> String? {
+        //1
         if operand1String.isEmpty, operand2String.isEmpty, operatorType == nil {
             return "0"
         } else {
+            //2
             var calculatedAnswer: String
-            
+            //3
             if let num1 = Double(operand1String), let num2 = Double(operand2String) {
-                
+                //4
                 switch operatorType {
                 case .addition? :
                     calculatedAnswer = String(num1 + num2)
@@ -64,7 +71,7 @@ class CalculatorBrain {
                 default :
                     return nil}} else {
                 return nil
-            }
+            } //5
             operand1String = calculatedAnswer
             operand2String = ""
             return calculatedAnswer

4f9b246787d90fd90da5b940e88e9794e2c857d9
Craig Swanson <cswanson02@me.com>
2019-09-15 22:59:31 -0500

    my calculator far from finished. I feel like a failure.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,10 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case division = "÷"
+    case multiplication = "×"
+    case subtraction = "−"
+    
 }
 
 class CalculatorBrain {
@@ -18,14 +22,26 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType == nil {
+            operand1String = operand1String + digit
+            return operand1String
+        } else {
+            operand2String = operand2String + digit
+            return operand2String
+        }
     }
     
     func setOperator(_ operatorString: String) {
-        
+        self.operatorType = OperatorType(rawValue: operatorString)
     }
     
-    func calculateIfPossible() -> String? {
-        
+    func calculateIfPossible(_ firstPart: String, _ secondPart: String, operator: String) -> String? {
+        /*
+         this will be called by the equalTapped.
+         if the operator type is .addition then perform addition on operand1String and operand2Sting
+         if it's .division then perform division on the two strings
+         set the result equal to a variable and return the variable as a string
+ */
+        return ""
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -10,33 +10,110 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var brain: CalculatorBrain?
+    
     @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var decimalButton: UIButton!
+    
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var timesButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var currentBrain = brain
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+ 
+        var currentNumber: String?
+            let index = sender.tag
+            switch index {
+            case 0:
+                currentNumber = zeroButton.titleLabel?.text
+            case 1:
+                currentNumber = oneButton.titleLabel?.text
+            case 2:
+                currentNumber = twoButton.titleLabel?.text
+            case 3:
+                currentNumber = threeButton.titleLabel?.text
+            case 4:
+                currentNumber = fourButton.titleLabel?.text
+            case 5:
+                currentNumber = fiveButton.titleLabel?.text
+            case 6:
+                currentNumber = sixButton.titleLabel?.text
+            case 7:
+                currentNumber = sevenButton.titleLabel?.text
+            case 8:
+                currentNumber = eightButton.titleLabel?.text
+            case 9:
+                currentNumber = nineButton.titleLabel?.text
+            case 10:
+                currentNumber = decimalButton.titleLabel?.text
+            default:
+                currentNumber = nil
+        }
+
+        if let passedNumber = currentNumber {
+            let textToPrint = brain?.addOperandDigit(passedNumber)
+            outputLabel.text = textToPrint
     }
-    
-    @IBAction func operatorTapped(_ sender: UIButton) {
+       
+    }
         
+    @IBAction func operatorTapped(_ sender: UIButton) {
+        brain?.operand1String = (outputLabel.text ?? "0")
+        var currentOperator: String?
+        let index = sender.tag
+        switch index {
+        case 1:
+            currentOperator = plusButton.titleLabel?.text
+        case 2:
+            currentOperator = minusButton.titleLabel?.text
+        case 3:
+            currentOperator = timesButton.titleLabel?.text
+        case 4:
+            currentOperator = divideButton.titleLabel?.text
+        default:
+            currentOperator = nil
+        }
+        
+        if let passedOperator = currentOperator {
+            brain?.setOperator(passedOperator)
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        let textToPrint = brain?.calculateIfPossible()
+        outputLabel.text = textToPrint
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
         
+        clearTransaction()
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        outputLabel.text = "0"
+        let newBrain = CalculatorBrain()
+        brain = newBrain
     }
 }

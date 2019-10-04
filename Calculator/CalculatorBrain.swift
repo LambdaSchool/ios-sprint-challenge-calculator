cdd625698bad7aa8a74b690927c2b45aceea62df
Mary Angelique A. Abacajan <mangelique9800@gmail.com>
2019-10-04 14:40:49 -0400

    Completed First Sprint Challenge - Calculator

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
 }
 
 class CalculatorBrain {
@@ -17,15 +20,57 @@ class CalculatorBrain {
     var operand2String = ""
     var operatorType: OperatorType?
     
+//     cannot put (number) gives you an error ==== put (digit) instead
+    // Use if else statement
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType == nil {
+            operand1String.append(digit)
+            return operand1String
+        } else {
+            operand2String.append(digit)
+            return operand2String
+        }
     }
+// cannot use _____ else {return}
+//    Instead, use {
+//    } else {
+//      __________
+//    return
+//    }
     
+    // insted of using operand1String or operand2String, use a string such as "operatorString" because "rawValue" always need a string
     func setOperator(_ operatorString: String) {
-        
+        operatorType = OperatorType(rawValue: operatorString)
     }
     
+//    Use an if else statement and SWITCH statement
     func calculateIfPossible() -> String? {
-        
+        if operand1String.isEmpty || operand2String.isEmpty || (operatorType != nil) {
+            return ""
+        } else {
+            var operationFunction: String
+            if let firstOperand = Double(operand1String), let secondOperand = Double(operand2String) {
+                
+                switch operatorType {
+                case .addition:
+                    operationFunction = String(firstOperand + secondOperand)
+                case .subtraction:
+                    operationFunction = String(firstOperand - secondOperand)
+                case .multiplication:
+                    operationFunction = String(firstOperand * secondOperand)
+                case .division:
+                    if secondOperand == 0 {
+                        return "Unable to calculate"
+                    } else {
+                        operationFunction = String(firstOperand / secondOperand)
+                    }
+                default:
+                    print("Dafault")
+                    }
+                }
+            }
     }
+    return String
+    
 }
+

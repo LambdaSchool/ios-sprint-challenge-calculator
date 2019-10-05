d5f416084496efe9ff6e8a56d9a1f99bc0dfeb1e
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2019-10-04 20:31:39 -0400

    can press buttons and see numbers and decimal point

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -28,10 +28,12 @@ class CalculatorBrain {
     func addOperandDigit(_ digit: String) -> String {
             if operatorType == nil {
                 operand1String.append(digit)
+                return operand1String
             } else {
-                _ = "\(operand2String)"
+                operand2String.append(digit)
+                return operand2String
             }
-         return digit
+        
         }
        
     

15dc4e0c9e691a97869eb851a895b2dd65355b6d
isaacgrove <isaacgrove333@gmail.com>
2019-11-01 13:29:53 -0400

    initialized brain class, functionalized addOperandDigit

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
 }
 
 class CalculatorBrain {
@@ -18,7 +21,13 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType != nil {
+            operand2String = operand2String + "\(digit)"
+            return operand1String
+        } else {
+            operand1String = operand1String + "\(digit)"
+            return operand1String
+        }
     }
     
     func setOperator(_ operatorString: String) {
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -13,14 +13,21 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
+        var brain = CalculatorBrain()
+        
         super.viewDidLoad()
     }
+    
+    var brain: CalculatorBrain?
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
+        guard let operandButtonText = sender.titleLabel?.text else {return}
         
-    }
+        outputLabel.text = brain?.addOperandDigit(operandButtonText)
+        }
+    
     
     @IBAction func operatorTapped(_ sender: UIButton) {
         
@@ -31,12 +38,15 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
+        outputLabel.text = "0"
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = nil
     }
+    
+    // ViewController bracket next line
 }

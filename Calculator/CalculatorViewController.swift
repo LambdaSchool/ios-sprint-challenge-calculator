294c3b5ee9868686879f14c7ed7e367a4c053af2
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-01 10:15:07 -0700

    Added usability to decimal point and +/- button. Connected +/- button to my CalculatorViewController.

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -61,4 +61,24 @@ class CalculatorViewController: UIViewController {
         brain?.operand2String = ""
         brain?.operatorType = nil
     }
+    @IBAction func valueSwitch(_ sender: UIButton) {
+        switch brain?.operatorType {
+        case nil:
+            if brain?.operand1String == "" {
+                break
+            } else {
+                let currentValue = brain?.operand1String
+                brain?.operand1String = ("-" + currentValue!)
+                outputLabel.text = brain?.operand1String
+            }
+        default:
+            if brain?.operand2String == "" {
+                break
+            } else {
+                let currentValue = brain?.operand2String
+                brain?.operand2String = ("-" + currentValue!)
+                outputLabel.text = brain?.operand2String
+            }
+        }
+    }
 }

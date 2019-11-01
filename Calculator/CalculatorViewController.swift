0a1a11db6e458a250538157fcf9537ee3ed686c7
Alexander Supe <alex.m.supe@gmail.com>
2019-11-01 16:37:44 +0100

    Update CalculatorViewController.swift
    Basic functionality added

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
+var brain: CalculatorBrain?
+
 class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let newBrain: CalculatorBrain = CalculatorBrain()
+        brain = newBrain
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        if let unwrappedOperand = sender.currentTitle{
+            outputLabel.text = brain?.addOperandDigit(unwrappedOperand)
+        }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
@@ -31,12 +37,13 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
+        outputLabel.text = "0"
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = CalculatorBrain()
     }
 }

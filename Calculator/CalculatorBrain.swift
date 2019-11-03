967d29a4a7fd1e9e1540a57d26960028d09f240c
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2019-11-03 22:35:07 +0100

    finished

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,24 +8,4 @@
 
 import Foundation
 
-enum OperatorType: String {
-    case addition = "+"
-}
 
-class CalculatorBrain {
-    var operand1String = ""
-    var operand2String = ""
-    var operatorType: OperatorType?
-    
-    func addOperandDigit(_ digit: String) -> String {
-        
-    }
-    
-    func setOperator(_ operatorString: String) {
-        
-    }
-    
-    func calculateIfPossible() -> String? {
-        
-    }
-}

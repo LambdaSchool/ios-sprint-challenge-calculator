996d67fe60edd81e25ffd52c0c6a5d496482de2a
kensam1 <samarakodykennedy@gmail.com>
2019-11-09 00:56:40 -0500

    Kennedy Samarakody first sprint

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,10 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
+
 }
 
 class CalculatorBrain {
@@ -18,14 +22,50 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
+        switch(operatorType){
+        case .addition:
+            operand2String.append(digit)
+        case .subtraction:
+            operand2String.append(digit)
+        case .multiplication:
+            operand2String.append(digit)
+        case .division:
+            operand2String.append(digit)
+        default:
+            operand1String.append(digit)
+        }
+        return operand2String
         
     }
     
     func setOperator(_ operatorString: String) {
-        
+        operatorType = OperatorType(rawValue: operatorString)
     }
     
     func calculateIfPossible() -> String? {
-        
-    }
+        var result: String
+        if operand1String != nil && operand2String != nil {
+            if operatorType != nil {
+                var num1 = Double(operand1String)
+                var num2 = Double(operand2String)
+                switch(operatorType) {
+                case .addition:
+                    result = num1 + num2
+                case .subtraction:
+                    result = num1 - num2
+                case .division:
+                    if let num2 == "0" {
+                        print("Error")
+                    } else {
+                    result = num1 / num2
+                    }
+                case .multiplication:
+                    result = num1 * num2
+                    
+                }
+            }
+            
+        }
+        return result
+    } 
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -14,29 +14,41 @@ class CalculatorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        brain = CalculatorBrain()
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
+        if let numberLabel = sender.titleLabel?.text {
+            outputLabel.text = brain?.addOperandDigit(numberLabel)
+        }
         
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        if let operatorLabel = sender.titleLabel?.text {
+            outputLabel.text = brain?.setOperator(operatorLabel)
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        if let outputLabel.text != nil {
+            brain?.calculateIfPossible()
+        }
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
+        clearTransaction()
+               outputLabel.text = nil
+            
         
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = nil
     }
+    var brain: CalculatorBrain?
 }

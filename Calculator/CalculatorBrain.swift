24cffe3ddbff119d4ef84ece3d108c8ec8518dc9
Osha Washington <washingtonosha209@yahoo.com>
2019-11-08 21:01:56 -0800

    Got Something but not all of it. maybe next time

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtract = "−"
+    case multiply = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
@@ -18,14 +21,44 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType == nil {
+            operand1String.append(digit)
+            return operand1String
+        } else {
+            operand2String.append(digit)
+            return operand2String
+        }
     }
     
     func setOperator(_ operatorString: String) {
-        
+        operatorType = OperatorType(rawValue: operatorString)
     }
     
     func calculateIfPossible() -> String? {
-        
+        if operand1String.isEmpty || operand2String.isEmpty || operatorType == nil {
+            return ""
+        } else {
+            var completedEquation: String
+            
+            if let firstOperand = Double(operand1String), let secondOperand = Double(operand2String) {
+                switch operatorType {
+                case .addition? :
+                    completedEquation = String(firstOperand + secondOperand)
+                case .subtract? :
+                    completedEquation = String(firstOperand - secondOperand)
+                case .multiply? :
+                    completedEquation = String(firstOperand * secondOperand)
+                case .division? :
+                    if secondOperand == 0 {
+                        return "Error"
+                    } else {
+                        completedEquation = String(firstOperand / secondOperand)
+                    }
+                default :
+                    return nil
+                }
+                
+            } else { return nil }
+            return completedEquation    }
     }
 }

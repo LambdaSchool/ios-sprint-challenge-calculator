7e59e70cf1b0c0baf5972aa17ebc9c35d27c1279
sal562 <salmanamer@gmail.com>
2019-11-09 11:23:26 +0700

    finished setOperator

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -35,14 +35,22 @@ class CalculatorBrain {
     }
     
     func setOperator(_ operatorString: String) {
-        
+        operatorType = OperatorType(rawValue: operatorString)
     }
     
     func calculateIfPossible() -> String? {
     }
 }
 
-
+//In setOperator, convert the operatorString passed into this method into an instance of your OperatorType enum, and then assign it to your operatorType property.=====done
+//In calculateIfPossible, hopefully you can calculate an answer based on the data you've stored so far in the brain.
+//Start by checking that the two operand strings are not empty, as "" wouldn't turn into a valid number.
+//Then, check that the operatorType variable isn't nil, since you need a valid operator to perform the arithmetic.
+//Convert the two operand strings into Doubles.
+//With all that setup done, use the type you found in step 2 to decide which kind of arithmetic you'll perform on the operands. Since you have several possible codepaths and only want to execute one of them at runtime, a switch is likely a good choice here. 😉
+//For each kind of arithmetic, use your two operand Doubles to perform either addition, subtraction, multiplication, or division (remember you can't divide-by-zero, so make sure to check for that before actually doing the math to avoid the app crashing. If you do encounter that, simply return "Error" for your answer).
+//Whatever your result is from the above arithmetic, return the answer as a String.
+//If you can't do the calculation because you're missing some piece of data, return nil.
 
 
 //In CalculatorBrain.swift:

dc9e748843aa9e3c98f7c3502a673c48b78795ba
Joshua Rutkowski <jr.rtkski@gmail.com>
2019-11-10 22:01:24 -0500

    Stretch goal: Updated `addOperandDigit` to ignore additional decimal points.

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -22,9 +22,15 @@ class CalculatorBrain {
     
     func addOperandDigit(_ digit: String) -> String {
         if operatorType == nil {
+            if operand1String.contains(".") && digit == "." {
+                return operand1String
+            }
             operand1String.append(digit)
             return operand1String
         } else {
+            if operand2String.contains(".") && digit == "." {
+                return operand2String
+            }
             operand2String.append(digit)
             return operand2String
         }

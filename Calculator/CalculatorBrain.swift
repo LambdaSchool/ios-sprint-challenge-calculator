d25e5b4f82fc95a7c27d16ced92475c2e529a2ab
alfcolon <alfcolon@gmail.com>
2019-11-10 20:22:32 -0800

    MVP

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -1,4 +1,4 @@
-//
+// GET FUNKY WITH IT!
 //  CalculatorBrain.swift
 //  Calculator
 //
@@ -7,25 +7,71 @@
 //
 
 import Foundation
-
+/* Above the class, look for the enum called `OperatorType`. It will help you decide which type of calculation to perform. One case is already created for you, but you'll need to add the other three: `subtraction`, `multiplication`, and `division`. Notice the raw value type of the enum is `String`, which means each case can have a separate value that is a `String`. Go look at the operator buttons in the storyboard. Notice they use different symbols from `+, -, *, /`. Copy the following symbols from this document for addition, subtraction, multiplication, and division: `+, −, ×, ÷`. Place them after each corresponding case (use the addition case as a model). This mapping of case to symbol will let you determine what operation the user wants to perform from the symbol string on the button. */
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
+    /* Inside the `CalculatorBrain` class, take a look at the 3 properties already declared for you. The first two will hold the values of the operands (so for example, the `2` and the `4` in `2 + 4`). The last property holds an instance of `OperatorType` and represents the kind of operation you'll be calculating (addition, subtraction, multiplication, or division). */
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
     
+    // add case for if operator is nil and (operand1 & operand2) exists to clear operand2 and start placing operand in operand1
     func addOperandDigit(_ digit: String) -> String {
+    /* In `addOperandDigit`, you have to figure out first which operand the passed-in digit belongs to. */
+        /* If the user has entered an operator already...*/
+        if let _ = operatorType {
+            /* CONTINUED: the `operatorType` you defined above will have a value. Use the presence or absence of an operator to decide where this `digit` belongs.*/
+            operand2String.append(digit)
+            return operand2String
+        }
+        /* Append the digit string to the end of either `operand1String` or `operand2String`*/
+        operand1String.append(digit)
+        /* Return that string to the caller of this method. */
+        return operand1String
         
     }
     
     func setOperator(_ operatorString: String) {
-        
+        /* In `setOperator`, convert the `operatorString` passed into this method into an instance of your `OperatorType` enum, */
+        if let selectedOperator: OperatorType = OperatorType(rawValue: operatorString) {
+            /* and then assign it to your `operatorType` property. */
+            operatorType = selectedOperator
+        }
     }
-    
+    /* In `calculateIfPossible`, hopefully you can calculate an answer based on the data you've stored so far in the brain.*/
     func calculateIfPossible() -> String? {
-        
+        /* Start by checking that the two operand strings are not empty, as `""` wouldn't turn into a valid number. Then, check that the `operatorType` variable isn't nil, since you need a valid operator to perform the arithmetic.*/
+        /* Convert the two operand strings into `Double`s. */
+        guard let operand1Double = Double(operand1String) else { return nil }
+        guard let operand2Double = Double(operand2String) else { return nil }
+        guard let selectedOperator = operatorType else { return nil }
+        /* With all that setup done, use the type you found in step 2 to decide which kind of arithmetic you'll perform on the operands. Since you have several possible codepaths and only want to execute one of them at runtime, a `switch` is likely a good choice here. 😉 */
+        switch selectedOperator {
+            /* For each kind of arithmetic, use your two operand `Double`s to perform either addition, subtraction, multiplication, or division (remember you can't divide-by-zero, so make sure to check for that before actually doing the math to avoid the app crashing. If you do encounter that, simply return `"Error"` for your answer).*/
+            /* Whatever your result is from the above arithmetic, return the answer as a `String`.*/
+            case .addition:
+               let finalAnswerDouble = operand1Double + operand2Double
+               return String(finalAnswerDouble)
+            case .subtraction:
+               let finalAnswerDouble = operand1Double - operand2Double
+               return String(finalAnswerDouble)
+            case .multiplication:
+               let finalAnswerDouble = operand1Double * operand2Double
+               return String(finalAnswerDouble)
+            case .division:
+                /* avoid dividing by zero*/
+                if operand2Double != 0 {
+                    let finalAnswerDouble = operand1Double / operand2Double
+                    return String(finalAnswerDouble)
+               } else {
+                    return "Error"
+               }
+        }
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -1,4 +1,4 @@
-//
+// GET FUNKY WITH IT!
 //  ViewController.swift
 //  Calculator
 //
@@ -9,34 +9,51 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-    
+    /* Create a property called `brain` of type `CalculatorBrain`. This object will hold the information related to the current calculation. A basic calculation consists of 2 operands and an operator (e.g. 2, 4, and +, or 2 + 4). Set the variable to be optional and do _not_ give it an initial value. This variable will get set and re-set with each transaction, so it needs to be optional. */
+    var brain: CalculatorBrain?
     @IBOutlet weak var outputLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        /* In `viewDidLoad`, initialize a new `CalculatorBrain` object
+         and assign it to the above property.*/
+        brain = CalculatorBrain()
     }
-    
+
     // MARK: - Action Handlers
-    
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        /* In `operandTapped`, go to the storyboard to see what buttons this action is connected to. Since it is connected to multiple buttons, you'll need to extract the `text` property from the button. It's likely going to require using the `if-let` technique to unwrap an optional. You should end up with a `String` which contains the text displayed on the button. This is the digit you need to add to your transaction. */
+        if let operandString = sender.titleLabel?.text {
+            /* Once you have the digit string, call `addOperandDigit` on your brain and pass in the digit string. That method returns the string you need to display on the screen, so assign the return value from that method to the `text` property of the `outputLabel`.*/
+             outputLabel.text = brain?.addOperandDigit(operandString)
+        }
     }
-    
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        /* `operatorTapped` follows virtually the same flow as the `operandTapped` method. */
+        if let operatorString = sender.titleLabel?.text {
+                 brain?.setOperator(operatorString)
+             }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        /*  In `equalTapped`, hopefully the user has entered everything they need to complete a mathematical expression. From here, you'll need to call `calculateIfPossible` on your brain object. That method will return a solution string to be displayed in your `outputLabel`, but if you look at the return type, it's actually a `String?`. Meaning you'll need to use another `if-let` to ensure the value returned isn't `nil` before displaying it in the label. */
+        if let answerString = brain?.calculateIfPossible() {
+            outputLabel.text = answerString
+        }
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        /* In `clearTapped`, the user is requesting to start over with a brand new transaction, so the two following steps need to be added to "clear" your calculator: */
+        /* Call the `clearTransaction` method. */
+        clearTransaction()
+        /* The `outputLabel` needs to be reset. What would be a good "default" value for the calculator screen (look at Apple's built-in calculator app or a real calculator for help)? */
+        outputLabel.text = "0"
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        /* In `clearTransaction`, you need to reset the `brain` object. Meaning you need to throw away the existing transaction data and create a space for a new transaction to be stored. Look at the declaration of this property at the top of your class. What would be the most efficient way to "discard" and initialize a new calculation? */
+        brain = nil
+        brain = CalculatorBrain()
     }
 }

c47a602e9c93704083b8b7bf8dc4a2614f853e25
William Castillo <calitato30@gmail.com>
2019-11-11 19:06:18 -0500

    IBActions

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -20,7 +20,10 @@ class CalculatorViewController: UIViewController {
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        guard let value = sender.titleLabel?.text else {
+            return
+        }
+        outputLabel.text = brain?.addOperandDigit(value)
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
@@ -28,6 +31,8 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
+        clearTransaction()
+        outputLabel.text = "0"
         
     }
     
@@ -38,6 +43,9 @@ class CalculatorViewController: UIViewController {
     // MARK: - Private
     
     private func clearTransaction() {
+        brain?.operand1String = ""
+        brain?.operand2String = ""
+        brain?.operatorType = nil
         
     }
 }

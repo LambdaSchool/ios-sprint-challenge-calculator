151b04eae63029139c7451f33d83d547276e1de8
Waseem Idelbi <idelbiw@gmail.com>
2019-11-12 21:08:43 -0500

    it works but the minus doesn't work for some reason

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -10,33 +10,45 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    @IBOutlet weak var outputLabel: UILabel!
+    var brain: CalculatorBrain?
     
+    @IBOutlet weak var outputLabel: UILabel!
+
     override func viewDidLoad() {
+        brain = CalculatorBrain()
         super.viewDidLoad()
+        
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        if let unwrappedNumber = sender.titleLabel?.text {
+            outputLabel.text = brain?.addOperandDigit(unwrappedNumber)
+        }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
+        if let opertator = sender.titleLabel?.text {
+            brain?.setOperator(opertator)
+        }
         
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        outputLabel.text = brain?.calculateIfPossible() 
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
+        outputLabel.text = "0"
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain?.operand1String = ""
+        brain?.operand2String = ""
+        brain?.operatorType = OperatorType(rawValue: "")
     }
 }

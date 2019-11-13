47535e518cf7443a92c26dd32336bdde51d2425d
DavidSchwinne <davidschwinne@gmail.com>
2019-11-13 09:47:11 +0900

    Submit Working code

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -8,48 +8,44 @@
 
 import UIKit
 
-
-
-
 class CalculatorViewController: UIViewController {
     
-    var brain: CalculatorBrain? //(operand) (operator) (operand)?
+    
+    @IBOutlet weak var outputLabel: UILabel!
+//    created brain of optional CalculatorBrain type
+    var brain: CalculatorBrain?
 
     
-    @IBOutlet weak var outputLabel: UILabel!,
-    
-    
     override func viewDidLoad() {
-        // initialize brain
-        brain = CalculatorBrain()
-        
         super.viewDidLoad()
+        // initialize brain / CalculatorBrain
+        brain = CalculatorBrain()
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
+//        set the .text of the titleLabel to "digit" and displayed the .text to the outputLabel, and set that equal to the brain .addOperandDigit.
         if let digit = sender.titleLabel?.text {
             outputLabel.text = brain?.addOperandDigit(digit)
         }
-        
-        
-  
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
+//        seet the .text of the titleLabel to operatorString and setOperator of operatorString with the button label pressed text.
         if let operatorString = sender.titleLabel?.text {
             outputLabel.text = brain?.setOperator(operatorString)
             }
     
     }
     @IBAction func equalTapped(_ sender: UIButton) {
-        if let equal = sender.titleLabel?.text {
-            outputLabel.text
-            brain?.calculateIfPossible()
+//        set the calculateIfPossible function to equal and displayed the result in the outputLabel .text
+        if let equal = brain?.calculateIfPossible() {
+            outputLabel.text = equal
         }
     }
     @IBAction func clearTapped(_ sender: UIButton) {
+//        runs the clearTransaction function, then sets the outputLabel .text to "0" on the display as seen on the mac calculator.
         clearTransaction()
         outputLabel.text = "0"
     }
@@ -57,9 +53,9 @@ class CalculatorViewController: UIViewController {
     // MARK: - Private
     
     private func clearTransaction() {
+//        sets the brain var to nil, and resets the CalculatorBrain function.
         brain = nil
         brain = CalculatorBrain()
-        
         }
 }
 

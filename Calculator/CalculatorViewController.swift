2262bfe99e3c17b0bb39cd19bde66c9c171d2caa
Tobi Tselios <tobi.tselios@gmail.com>
2019-12-08 17:54:31 +0000

    First part of CalculatorViewController redone

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -14,22 +14,10 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
-    @IBOutlet weak var zero: UIButton!
-    @IBOutlet weak var one: UIButton!
-    @IBOutlet weak var two: UIButton!
-    @IBOutlet weak var three: UIButton!
-    @IBOutlet weak var four: UIButton!
-    @IBOutlet weak var five: UIButton!
-    @IBOutlet weak var six: UIButton!
-    @IBOutlet weak var seven: UIButton!
-    @IBOutlet weak var eight: UIButton!
-    @IBOutlet weak var nine: UIButton!
-    @IBOutlet weak var dot: UIButton!
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         brain = CalculatorBrain()
     }
     
@@ -37,42 +25,8 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func operandTapped(_ sender: UIButton) {
         
-        zero.setTitle("0", for: .normal)
-        one.setTitle("1", for: .normal)
-        two.setTitle("2", for: .normal)
-        three.setTitle("3", for: .normal)
-        four.setTitle("4", for: .normal)
-        five.setTitle("5", for: .normal)
-        six.setTitle("6", for: .normal)
-        seven.setTitle("7", for: .normal)
-        eight.setTitle("8", for: .normal)
-        nine.setTitle("9", for: .normal)
-        dot.setTitle(".", for: .normal)
-        
-        if let unwrappedZero = zero {
-            print(unwrappedZero)
-        } else if let unwrappedOne = one {
-            print(unwrappedOne)
-        } else if let unwrappedTwo = two {
-            print(unwrappedTwo)
-        } else if let unwrappedThree = three {
-            print(unwrappedThree)
-        } else if let unwrappedFour = four {
-            print(unwrappedFour)
-        } else if let unwrappedFive = five {
-            print(unwrappedFive)
-        } else if let unwrappedSix = six {
-            print(unwrappedSix)
-        } else if let unwrappedSeven = seven {
-            print(unwrappedSeven)
-        } else if let unwrappedEight = eight {
-            print(unwrappedEight)
-        } else if let unwrappedNine = nine {
-            print(unwrappedNine)
-        } else if let unwrappedDot = dot {
-            print(unwrappedDot)
-        }
-
+        guard let operandString = sender.currentTitle else { return }
+        outputLabel.text = brain?.addOperandDigit(operandString)
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
@@ -91,6 +45,6 @@ class CalculatorViewController: UIViewController {
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = nil
     }
 }

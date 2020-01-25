5d055becafa741ea4b7020ccc67149c81548a0c2
Michael Kagan <mkagan532@gmail.com>
2020-01-25 14:54:31 -0500

    kinda functional

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,22 +10,70 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "x"
+    case division = "÷"
 }
 
 class CalculatorBrain {
     var operand1String = ""
     var operand2String = ""
-    var operatorType: OperatorType?
+    var operatorType: OperatorType? = nil
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operand1String != "" {
+            operand2String = digit
+            return operand2String
+        } else {
+            operand1String = digit
+            return operand1String
+        }
     }
     
     func setOperator(_ operatorString: String) {
-        
+        switch operatorString {
+        case "+":
+            operatorType = .addition
+        case "-":
+            operatorType = .subtraction
+        case "x":
+            operatorType = .multiplication
+        case "÷":
+            operatorType = .division
+        default:
+            return
+        }
     }
     
     func calculateIfPossible() -> String? {
+       if operand1String == ""
+        || operand2String == "" ||
+        operatorType == nil {
+        return "0"
+        }
         
+        
+        let operand1 = Double.init(operand1String)
+        let operand2 = Double.init(operand2String)
+        var result: Double
+        
+        switch operatorType {
+        case .addition:
+            result = (operand1 ?? 0.0) + (operand2 ?? 0.0)
+        case .subtraction:
+            if operand2 != 0 {
+                result = (operand1 ?? 0.0) - (operand2 ?? 0.0)
+            } else {return "error" }
+        case .multiplication:
+            result = (operand1 ?? 0.0) * (operand2 ?? 0.0)
+        case .division:
+            result = (operand1 ?? 0.0) / (operand2 ?? 0.0)
+        default:
+            return nil
+        }
+        operand1String = ""
+        operand2String = ""
+        operatorType = nil
+        return "\(result)"
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -14,31 +14,48 @@ class CalculatorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        brain = CalculatorBrain()
+        outputLabel.text = "0"
     }
     
-    var brain: CalculatorBrain = 
+    var brain: CalculatorBrain?
+    var display: String = ""
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        let button: String = sender.currentTitle ?? ""
+        if display == "" {
+            display = "\(button)"
+            outputLabel.text = display
+        } else {
+            display = "\(display)\(button)"
+            outputLabel.text = display
+        }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        let button: String = sender.currentTitle ?? ""
+        outputLabel.text = brain?.addOperandDigit(display)
+        display = ""
+        brain?.setOperator(button)
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        display = brain?.addOperandDigit(display) ?? ""
+        outputLabel.text = (brain?.calculateIfPossible() ?? display)
+        display = ""
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
+        outputLabel.text = "0"
+        display = ""
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = CalculatorBrain()
     }
 }

d06f792ae8d3ad816300d17f219339e9109af1cc
hai666l <0xconr@protonmail.com>
2020-01-24 20:50:35 -0600

    Completed CalculatorViewController.swift

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -8,22 +8,29 @@
 
 import UIKit
 
+var brain: CalculatorBrain?
+
 class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        brain = CalculatorBrain()
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        
+        if let operandText = sender.titleLabel?.text {
+            outputLabel.text = brain?.addOperandDigit(operandText)
+        }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        if let operatorText = sender.titleLabel?.text {
+            outputLabel.text = brain?.addOperandDigit(operatorText)
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
@@ -31,12 +38,13 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
+        outputLabel.text = "0"
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = nil
     }
 }

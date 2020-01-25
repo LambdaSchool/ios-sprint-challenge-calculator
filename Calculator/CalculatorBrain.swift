819078c5123981f79a604bc1dfb3dd50c1744d11
Matt Martindale <martindalematt18@yahoo.com>
2020-01-25 12:05:19 -0700

    verified what operator was chosen and set as operator type

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -32,7 +32,18 @@ class CalculatorBrain {
     }
     
     func setOperator(_ operatorString: String) {
-        
+        switch operatorString {
+        case "+":
+            operatorType = .addition
+        case "−":
+            operatorType = .subtraction
+        case "×":
+            operatorType = .multiplication
+        case "÷":
+            operatorType = .division
+        default:
+            operatorType = nil
+        }
     }
     
     func calculateIfPossible() -> String? {

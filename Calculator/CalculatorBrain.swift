ab00855865d362cf33c1076c62d7f433bc9873d2
kensam1 <samarakodykennedy@gmail.com>
2020-01-24 22:11:53 -0500

    KennedyS first sprint. IOS calculator

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,10 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
+    
 }
 
 class CalculatorBrain {
@@ -18,14 +22,52 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
+        if let mathOperator = operatorType {
+            operand1String.append(digit)
+            return operand1String
+        } else {
+            operand2String.append(digit)
+            return operand2String
+        }
+        
         
     }
     
     func setOperator(_ operatorString: String) {
-        
+        if let sign = OperatorType(rawValue: operatorString) {
+            operatorType = sign
+            
+        }
     }
     
     func calculateIfPossible() -> String? {
-        
+        if operand1String != "", operand2String != "" {
+            guard let operatorType = operatorType, let num1 = Double(operand1String), let num2 = Double(operand2String) else { return "Error" }
+            switch(operatorType){
+            case .addition:
+                return String(num1+num2)
+            case .subtraction:
+                return String(round((num2-num1)*10)/10)
+            case .multiplication:
+                return String(num1*num2)
+            case .division:
+                if num1 == 0 {
+                    return "Error"
+                } else {
+                    
+                    return String(num2/num1)
+                }
+            }
+        }
+        return nil
     }
+    
 }
+
+
+
+
+
+
+
+

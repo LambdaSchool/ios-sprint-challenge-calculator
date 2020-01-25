563e83ee83147e3bbb4b22bb22b66e5a91074d68
Libby Thomas <thomaslibby05@gmail.com>
2020-01-24 22:33:15 -0600

    Calculator In Progress

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
@@ -17,15 +20,30 @@ class CalculatorBrain {
     var operand2String = ""
     var operatorType: OperatorType?
     
+    
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType != nil {
+            operand2String.append(digit)
+            return operand2String
+        } else {
+            operand1String.append(digit)
+            return operand1String
+        }
     }
     
     func setOperator(_ operatorString: String) {
+        if let mathOperator = OperatorType(rawValue: operatorString) {
+            operatorType = mathOperator
+        }
         
     }
     
     func calculateIfPossible() -> String? {
-        
+        if operand1String != "" && if operand2String != "" && if operatorType != nil {
+            
+        }
     }
 }
+
+
+

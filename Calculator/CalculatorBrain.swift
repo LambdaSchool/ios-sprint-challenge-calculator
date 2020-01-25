e8232888bdb0e68008b0ae615990ea9c9778f70c
Claudia Maciel <clc_80@yahoo.com>
2020-01-24 18:47:40 -0800

    addOperandDigit function code added

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -21,8 +21,13 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
-        return "hello"
+        if operatorType != nil  {
+            operand2String += digit
+            return operand2String
+        } else {
+            operand1String += digit
+            return operand1String
+        }
     }
     
     func setOperator(_ operatorString: String) {

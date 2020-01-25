58527296ed09255836d22ade47b1f29c3c974ad5
Claudia Maciel <clc_80@yahoo.com>
2020-01-24 18:41:25 -0800

    enum completed

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
 }
 
 class CalculatorBrain {
@@ -18,6 +21,7 @@ class CalculatorBrain {
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
+        
         return "hello"
     }
     

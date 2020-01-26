d43f6494464e9a3c2f7f220b64281118d8c55173
kendra rayne <raynekendra@gmail.com>
2020-01-25 21:18:31 -0500

    getting closer!

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -11,7 +11,7 @@ import Foundation
 enum OperatorType: String {
     case addition = "+"
     case subtraction = "-"
-    case multiplication = "*"
+    case multiplication = "×"
     case division = "/"
 }
 
@@ -20,15 +20,53 @@ class CalculatorBrain {
     var operand2String = ""
     var operatorType: OperatorType?
     
+    
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType != nil {
+            operand1String.append(digit)
+            return operand1String
+        } else {
+            operand2String.append(digit)
+            return operand2String
+        }
     }
     
     func setOperator(_ operatorString: String) {
-        
+        operatorType = OperatorType(rawValue: operatorString)
     }
     
     func calculateIfPossible() -> String? {
-        
+        if operand1String.isEmpty, operand2String.isEmpty, operatorType == nil {
+            return "0"
+            // this is saying if nothing has been entered, the default value in the output screen will say 0 until it's changed.
+        } else {
+            var calculatedAnswer: String
+            if let num1 = Double(operand1String), let num2 = Double(operand2String) {
+                // type-casting is done here
+                switch operatorType {
+                case .addition?:
+                    calculatedAnswer = String(num1 + num2)
+                case .subtraction?:
+                    calculatedAnswer = String(num1 - num2)
+                case .multiplication?:
+                    calculatedAnswer = String(num1 * num2)
+                case .division?:
+                    calculatedAnswer = String(num1 / num2)
+                    
+                default: // I'm not gonna lie, I had nil } and had an error, so I tried that and it magically worked. Not sure if it's right? I've never seen that before.
+                    
+                    return nil}} else {
+                return nil
+            }
+            operand1String = calculatedAnswer
+            operand2String = ""
+            return calculatedAnswer
+        }
+    }
+    func clear() {
+        operand1String = ""
+        operand2String = ""
+        operatorType = nil
     }
 }
+// this makes it default back to 0 on the screen
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -13,39 +13,52 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
-
+    //refers to the label that shows you what you type in and what answer you get
+    
+    
     
     //MARK: Life-Cycle
     var brain: CalculatorBrain?
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         brain = CalculatorBrain()
-            super.viewDidLoad()
     }
+    //I'm not really sure what .viewDidLoad does. I'm gonna need to google this
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
-        if let OperatorType.addition
+        if let number = sender.currentTitle {
+            let operand = number
+            outputLabel.text = brain?.addOperandDigit(operand)
         }
-        
     }
     
-    
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        if let tappedOperatorButton = sender.titleLabel?.text {
+            brain?.setOperator(tappedOperatorButton)
+            outputLabel.text = tappedOperatorButton
+        }
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
-        
+        if let answer = brain?.calculateIfPossible() {
+            outputLabel.text = answer
+        }
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        clearTransaction()
+        outputLabel.text = "0"
     }
+    // assigns the outputLabel's text to 0 once clear is tapped.
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = CalculatorBrain()
+        brain?.clear()
     }
 }
+// Our initializer is put in again (which resets it) the ? checks if it's empty, if it is, it runs the clear code.

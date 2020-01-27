2a641ba1f8fd6c35558ff84b561ab23e92975be3
ldmayfield <ldonnamayfield@gmail.com>
2020-01-26 21:23:40 -0700

    0-9, buttons functional, view controller additions

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -9,23 +9,62 @@
 import Foundation
 
 enum OperatorType: String {
+    // Add operators
     case addition = "+"
-}
+    case subtraction = "−"
+    case multiplication = "×"
+    case division = "÷"
+ }
 
 class CalculatorBrain {
+    
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
     
     func addOperandDigit(_ digit: String) -> String {
-        
+        if operatorType == nil {
+            operand1String.append(contentsOf: digit)
+            return operand1String
+        } else {
+            operand2String.append(contentsOf: digit)
+            return operand2String
+        }
+    
     }
     
     func setOperator(_ operatorString: String) {
-        
+        // instance of OperatorType enum
+        operatorType = OperatorType(rawValue: operatorString)
     }
     
     func calculateIfPossible() -> String? {
+        var returnValue: String?
         
+        if operand1String == "" || operand2String == "" {
+            returnValue = "0"
+        }
+        
+        
+        guard let value1 = Double(operand1String) else { return nil }
+        guard let value2 = Double(operand2String) else { return nil }
+        
+        if let operatorType = operatorType {
+            switch operatorType {
+            case .addition:
+                returnValue = "\(value1 + value2)"
+            case .subtraction:
+                returnValue = "\(value1 - value2)"
+            case .multiplication:
+                returnValue = "\(value1 * value2)"
+            case .division:
+                returnValue = "\(value1 / value2)"
+            }
+        }
+        
+        if returnValue == nil {
+            returnValue = "0"
+        }
+        return returnValue
     }
 }
--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -8,25 +8,37 @@
 
 import UIKit
 
-var brain: CalculatorBrain?
+var brain: CalculatorBrain? //*1 is this already done?
+var oldValue: String?
 
 class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var outputLabel: UILabel!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        brain = CalculatorBrain() //*2 initialize new CalculatorBrain object
         
     }
     
     // MARK: - Action Handlers
     
     @IBAction func operandTapped(_ sender: UIButton) {
+        //*3 extract text property from button with if let
+        //* call addOperand Digit on brain(optional?)
+        // pass in digit string using outputLabel
         
+        if let noTapped = sender.titleLabel?.text {
+            outputLabel.text = brain?.addOperandDigit(noTapped)
+        }
     }
     
     @IBAction func operatorTapped(_ sender: UIButton) {
-        
+        if oldValue != nil {
+            brain?.operand1String = oldValue!
+            oldValue = nil
+        } 
     }
     
     @IBAction func equalTapped(_ sender: UIButton) {
@@ -34,12 +46,16 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
-        
+        // call clearTransaction Method to initialize new calculation
+        // reset outputLabel.text to blank
+        clearTransaction()
+        oldValue = nil
+        outputLabel.text = " "
     }
     
     // MARK: - Private
     
     private func clearTransaction() {
-        
+        brain = CalculatorBrain()
     }
 }

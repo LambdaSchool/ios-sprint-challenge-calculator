41cb742afc019ec44d46bdb91f50e513a4d1166d
Jorge Talavera <jorgetalavera02@gmail.com>
2020-01-26 19:30:35 -0800

    Added 3 more cases to the enum

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -10,6 +10,9 @@ import Foundation
 
 enum OperatorType: String {
     case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "/"
 }
 
 class CalculatorBrain {

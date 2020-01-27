a3fe27e257854ee9266b298a92ab0c15bdf4a98c
raynekendra <58447502+raynekendra@users.noreply.github.com>
2020-01-27 00:48:04 -0500

    Comments put in

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -14,12 +14,18 @@ enum OperatorType: String {
     case multiplication = "×"
     case division = "÷"
 }
+/* created other cases in the enum that's needed for the calcualtor. had to use the storyboard's calculator's operator texts 
+to make the calculator fully functional, so I copied and pasted from that into the rawValues' strings.
+*/
 
 class CalculatorBrain {
     var operand1String = ""
     var operand2String = ""
     var operatorType: OperatorType?
     
+    
+/* creates empty variables, just waiting for instances to be created!
+    */
     
     func addOperandDigit(_ digit: String) -> String {
         if operatorType != nil {
@@ -30,10 +36,15 @@ class CalculatorBrain {
             return operand2String
         }
     }
+ /* //this in thruth was me trying random things until something worked. the instructions were a little unlcear for me. 
+    I saw append, "if the user has entered an operator already", and "which operand the passed-in digit belongs to" and 
+    tried stuff. i will need to go over this one with someone.   
+    */
     
     func setOperator(_ operatorString: String) {
         operatorType = OperatorType(rawValue: operatorString)
     }
+    // makes an instance (is that the right word?) of the enumerator.
     
     func calculateIfPossible() -> String? {
         if operand1String.isEmpty, operand2String.isEmpty, operatorType == nil {
@@ -42,7 +53,7 @@ class CalculatorBrain {
         } else {
             var calculatedAnswer: String
             if let num1 = Double(operand1String), let num2 = Double(operand2String) {
-                // type-casting is done here
+                // type-casting is done here to convert the strings into doubles.
                 guard operatorType != nil else {
                     print("0")
                     return "0"
@@ -57,14 +68,22 @@ class CalculatorBrain {
                 case .division?:
                     calculatedAnswer = String(num2 / num1)
                     
-                default: // I'm not gonna lie, I had nil } and had an error, so I tried that and it magically worked. Not sure if it's right? I've never seen that before.
+                default:
                     
-                    return nil}} else {
+                    return nil } } else {
                 return nil
             }
+            
+           /* I love switch statements. they just look so clean. they're my favorite thing! so that was fun lol. 
+            but yeah, this is basically saying, "if user hit this("this" being one of the enums), perform this math. 
+            the code stops running until the used one is found and then performs the arithmetic given. 
+            I assigned the operand1&2Strings to num1&num2 so I could convert them into Doubles. 
+            */
+            
             operand1String = calculatedAnswer
             operand2String = ""
             return calculatedAnswer
+          
         }
     }
     func clear() {
